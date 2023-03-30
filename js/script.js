835f2e6c4c141358d2b782b48532f5afca45bb24
mai_b <maim181@outlook.com>
2023-03-30 10:38:43 +0100

    Added a keydown event to close modal with escape key and and refactored the code

--- js/script.js
+++ js/script.js
@@ -3,11 +3,35 @@ const modal = document.querySelector(".modal");
 const modalX = document.querySelector(".modal-x");
 
 button.addEventListener("click", function () {
-  modal.classList.add("show-modal");
-  button.innerText = "You’ve got this";
+  /* modal.classList.add("show-modal");
+  button.innerText = "You’ve got this"; */
+  openModal();
 });
 
 modalX.addEventListener("click", function () {
-  modal.classList.remove("show-modal");
-  button.innerText = "Who’s got this?";
+  /*  modal.classList.remove("show-modal");
+  button.innerText = "Who’s got this?"; */
+  closeModal();
 });
+
+// Create a Keydown Event Listener to close modal with the escape key
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    //console.log("this is the key");
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
+    }
+  }
+  closeModal();
+});
+
+/* Refactored code */
+const closeModal = function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
+const openModal = function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+};
